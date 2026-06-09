9029042737635f6423fef8d1c63af1b0c6ffb433
daniel.lawand <37334397+danlawand@users.noreply.github.com>
2026-06-09 17:10:56 -0300

    Add: swim-in-rising-water - submission-4

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        visited = set()
+        maxHeight = [[grid[0][0], 0, 0]]
+        directions = [[-1,0], [1,0], [0,1], [0,-1]]
+        # N = n**2
+        # O(4 + N * log N)
+        while maxHeight:
+            # O(log N)
+            t, r, c = heapq.heappop(maxHeight)
+            if r == n-1 and c == n-1:
+                return t
+            
+            for rDir, cDir in directions:
+                newR, newC = r+rDir, c+cDir
+                if newR < 0 or newC < 0 or newR >= n or newC >= n or (newR, newC) in visited:
+                    continue
+                # O(log N)
+                heapq.heappush(maxHeight, [max(t, grid[newR][newC]), newR, newC])
+                visited.add((newR, newC))
+        return -1
+            
+
+
